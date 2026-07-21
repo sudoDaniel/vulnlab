393f38d68d260d2bcc3b61eda2d6f2e88a8720ed
sudoDaniel <d.schjetne@proton.me>
2026-07-21 14:18:37 +0200

    draft of routes/queries

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def get_all():
+    return "This should list all posts in the database.\ntitle, username, created_at, pull via a JOIN"
+
+
+@app.route("/posts/<int:post_id>")
+def get_detailed_post(post_id: int):
+    return "a single post's detail page: the post itself, plus all its comments and each comment's author"
+
+
+@app.route("/users/<int:user_id>")
+def get_detailed_user(user_id: int):
+    return "a user's profile: username, email, and a list of posts they've authored"
 
 
 if __name__ == "__main__":
--- db/queries.py
+++ db/queries.py
@@ -5,6 +5,21 @@ from datetime import datetime
 from loguru import logger
 
 
+def query(conn: sqlite3.Connection, query: str):
+    logger.debug("Executing Query:\n")
+    print(f"\n{query}\n")
+    c = conn.cursor()
+    c.execute(query)
+
+    for row in c.fetchall():
+        print(row)
+
+    conn.commit()
+
+
+""" CREATING USERS, POSTS AND COMMENTS """
+
+
 def create_user(
     conn: sqlite3.Connection,
     username: str,
@@ -67,13 +82,24 @@ def create_comment(
     return c.lastrowid
 
 
-def query(conn: sqlite3.Connection, query: str):
-    logger.debug("Executing Query:\n")
-    print(f"\n{query}\n")
-    c = conn.cursor()
-    c.execute(query)
+""" GET QUERIES """
 
-    for row in c.fetchall():
-        print(row)
 
-    conn.commit()
+def get_all_posts(conn: sqlite3.Connection):
+    logger.debug("Fetching all posts in the database")
+    pass
+
+
+def get_post_by_id(conn: sqlite3.Connection, post_id: int):
+    logger.debug("Fetching details on post_id: {}", post_id)
+    pass
+
+
+def get_comments_for_post(conn: sqlite3.Connection, post_id):
+    logger.debug("Fetching comments on post_id: {}", post_id)
+    pass
+
+
+def get_user_by_id(conn: sqlite3.Connection, user_id: int):
+    logger.debug("Fetching details on user_id: {}", user_id)
+    pass
